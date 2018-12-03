11eaa203c38afac6e1c4097eca62066aaf78566c
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-12-03 23:51:30 +0200

    Auto stash before merge of "develop" and "master"

--- tests/Unit/SimpleFormatterTest.php
+++ tests/Unit/SimpleFormatterTest.php
@@ -8,6 +8,7 @@
 namespace Tests;
 
 
+use const AlecRabbit\Constants\UNIT_MICROSECONDS;
 use AlecRabbit\SimpleFormatter;
 use PHPUnit\Framework\TestCase;
 
@@ -17,24 +18,22 @@ class SimpleFormatterTest extends TestCase
      * @test
      * @dataProvider  dataProvider
      * @param $expected
-     * @param $bytes
-     * @param $unit
-     * @param $decimals
+     * @param $args
      */
-    public function formatterBytes($expected, $bytes, $unit, $decimals): void
+    public function formatterBytes($expected, $args): void
     {
-        $this->assertEquals($expected, SimpleFormatter::bytes($bytes, $unit, $decimals));
+        $this->assertEquals($expected, SimpleFormatter::bytes(...$args));
     }
 
     public function dataProvider(): array
     {
         return [
-            ['1.0KB', 1035, 'KB', 1],
-            ['1.01KB', 1035, 'KB', 2],
-            ['1.011KB', 1035, 'KB', 3],
-            ['1.0107KB', 1035, 'KB', 4],
-            ['1.01074KB', 1035, 'KB', 5],
-            ['1.01KB', 1035, 'KB', null],
+            ['1.0KB', [1035, 'KB', 1]],
+            ['1.01KB', [1035, 'KB', 2]],
+            ['1.011KB', [1035, 'KB', 3]],
+            ['1.0107KB', [1035, 'KB', 4]],
+            ['1.01074KB', [1035, 'KB', 5]],
+            ['1.01KB', [1035, 'KB']],
         ];
     }
 
@@ -42,25 +41,23 @@ class SimpleFormatterTest extends TestCase
      * @test
      * @dataProvider  dataProviderTime
      * @param $expected
-     * @param $value
-     * @param $unit
-     * @param $decimals
+     * @param $args
      */
-    public function formatterTime($expected, $value, $unit, $decimals): void
+    public function formatterTime($expected, $args): void
     {
-        $this->assertEquals($expected, SimpleFormatter::time($value, $unit, $decimals));
+        $this->assertEquals($expected, SimpleFormatter::time(...$args));
     }
 
     public function dataProviderTime(): array
     {
         return [
-            ['10350μs', 0.01035, UNIT_MICROSECONDS, 1],
-            ['10.4ms', 0.01035, null, 1],
-            ['10.3ms', 0.010349, null, 1],
-            ['10.35ms', 0.01035, null, 2],
-            ['10.351ms', 0.01035123, null, 3],
-            ['10.3532ms', 0.01035321, null, 4],
-            ['10.35123ms', 0.01035123, null, 5],
+            ['10350μs', [0.01035, UNIT_MICROSECONDS, 1]],
+            ['10.4ms', [0.01035, null, 1]],
+            ['10.3ms', [0.010349, null, 1]],
+            ['10.35ms',[ 0.01035, null, 2]],
+            ['10.351ms',[ 0.01035123, null, 3]],
+            ['10.3532ms',[ 0.01035321, null, 4]],
+            ['10.35123ms',[ 0.01035123, null, 5]],
         ];
     }
 }
