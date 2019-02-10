dc95cbd9fc3940264c161fad9f62bd2547af6c8b
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-02-10 22:37:23 +0200

    Caller class

--- examples/Caller/caller.php
+++ examples/Caller/caller.php
@@ -10,6 +10,10 @@ class SomeStupidClass
     {
         someFunction();
     }
+    public static function usesSomeFunctionStatic(): void
+    {
+        someFunction();
+    }
 }
 
 function someFunction()
@@ -17,11 +21,32 @@ function someFunction()
     throw new \RuntimeException(Caller::method() . ' called this function');
 }
 
+function usesSomeFunction(): void
+{
+    someFunction();
+}
+
+
+try {
+    usesSomeFunction();
+} catch (\Exception $e) {
+    dump($e->getMessage());
+    var_dump($e);
+}
+
 
 $s = new SomeStupidClass();
 try {
     $s->usesSomeFunction();
 } catch (\Exception $e) {
+    dump($e->getMessage());
+    var_dump($e);
+}
+
+try {
+    SomeStupidClass::usesSomeFunctionStatic();
+} catch (\Exception $e) {
+    dump($e->getMessage());
     var_dump($e);
 }
 
--- src/Accessories/Caller.php
+++ src/Accessories/Caller.php
@@ -7,16 +7,26 @@ namespace AlecRabbit\Accessories;
 /** @experimental */
 class Caller
 {
-    public static function method(int $depth = 2): string
+    public static function method(int $depth = 3): string
     {
-        $caller = (new \Exception())->getTrace()[$depth];
-        $r = $caller['function'] . '()';
+        $caller = static::get($depth);
+        $r = '';
+        $function = $caller['function'] . '()';
         if (isset($caller['class'])) {
-            $r .= ' in ' . $caller['class'];
+            $type = $caller['type'] ?? '';
+            $r .= $caller['class'] . $type . $function;
+        } else {
+            $r .= $function;
         }
         if (isset($caller['object'])) {
-            $r .= ' (' . get_class($caller['object']) . ')';
+            $r .= ' (' . \get_class($caller['object']) . ')';
         }
         return $r;
+    }
+
+    public static function get(int $depth = 2): array
+    {
+        return
+            (new \Exception())->getTrace()[$depth];
     }
 }
