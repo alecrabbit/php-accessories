e7f49bc39fc87876c98572c2a6f54d49f1c6e79e
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-02-11 22:04:09 +0200

    wp

--- examples/Caller/caller.php
+++ examples/Caller/caller.php
@@ -4,12 +4,21 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use AlecRabbit\Accessories\Caller;
 
+class CallingClass {
+    public function __construct()
+    {
+        $c = new SomeStupidClass();
+        $c->usesSomeFunction();
+    }
+}
+
 class SomeStupidClass
 {
     public function usesSomeFunction(): void
     {
         someFunction();
     }
+
     public static function usesSomeFunctionStatic(): void
     {
         someFunction();
@@ -31,7 +40,14 @@ try {
     usesSomeFunction();
 } catch (\Exception $e) {
     dump($e->getMessage());
-    var_dump($e);
+//    var_dump($e);
+}
+
+try {
+    $c = new CallingClass();
+} catch (\Exception $e) {
+    dump($e->getMessage());
+//    var_dump($e);
 }
 
 
@@ -40,14 +56,14 @@ try {
     $s->usesSomeFunction();
 } catch (\Exception $e) {
     dump($e->getMessage());
-    var_dump($e);
+//    var_dump($e);
 }
 
 try {
     SomeStupidClass::usesSomeFunctionStatic();
 } catch (\Exception $e) {
     dump($e->getMessage());
-    var_dump($e);
+//    var_dump($e);
 }
 
 
