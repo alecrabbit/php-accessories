b8a074327c8d3866d3a39bdd181f3940e81304d9
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-28 09:38:29 +0300

    tests

--- tests/Unit/CallerDataTest.php
+++ tests/Unit/CallerDataTest.php
@@ -2,15 +2,47 @@
 
 namespace AlecRabbit\Tests\Accessories;
 
+use AlecRabbit\Accessories\Caller\CallerData;
+use AlecRabbit\Reports\Contracts\ReportableInterface;
+use AlecRabbit\Reports\Core\AbstractReport;
+use AlecRabbit\Reports\Core\Reportable;
+use AlecRabbit\Reports\Contracts\ReportInterface;
 use PHPUnit\Framework\TestCase;
 
 class CallerDataTest extends TestCase
 {
-    // TODO add tests
-
     /** @test */
-    public function one(): void
+    public function buildOn(): void
     {
-        $this->assertTrue(true);
+        $report = new CallerData();
+        $this->expectException(\InvalidArgumentException::class);
+        $report->buildOn(
+            new class extends Reportable
+            {
+                protected function createEmptyReport(): ReportInterface
+                {
+                    return
+                        new class extends AbstractReport
+                        {
+                            /**
+                             * @return string
+                             */
+                            public function __toString(): string
+                            {
+                                return '';
+                            }
+
+                            /**
+                             * @param ReportableInterface $reportable
+                             * @return ReportInterface
+                             */
+                            public function buildOn(ReportableInterface $reportable): ReportInterface
+                            {
+                                return $this;
+                            }
+                        };
+                }
+            }
+        );
     }
 }
